5f97adac17def61cd512ff2ac3d23b1aa0fd986d
Jonexy01 <jonexy01@gmail.com>
2022-05-11 00:31:10 +0100

    second commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xffE5E5E5),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(66, 193, 0, 0),
+            child: Text(
+              'Set up your password',
+              style: TextStyle(
+                fontSize: 18,
+                // fontFamily: 'Mulish',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(66, 226, 0, 0),
+            child: Text(
+              'Enter your details to login to your account',
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: Color(0xff787D76),
+              ),
+            ),
+          ),
+          Container(
+            width: size.width * 0.84,
+            height: 50,
+            margin: EdgeInsets.fromLTRB(size.width * 0.08, 291, 0, 0),
+            // decoration: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(8),
+            // ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                // filled: true,
+                // fillColor: Color(0xffFFFFFF),
+                hintText: 'New Password',
+                suffixIcon: Icon(
+                  Icons.visibility, 
+                  color: Colors.black,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Color(0xffD2D3D1),
+                  ),
+                ),
+              ),
+            )
+          ),
+          Container(
+            width: size.width * 0.84,
+            height: 50,
+            margin: EdgeInsets.fromLTRB(size.width * 0.08, 382, 0, 0),
+            // decoration: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(8),
+            // ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                // filled: true,
+                // fillColor: Color(0xffFFFFFF),
+                hintText: 'Confirm Password',
+                suffixIcon: Icon(
+                  Icons.visibility, 
+                  color: Colors.black,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                  borderSide: const BorderSide(
+                    width: 1,
+                    color: Color(0xffD2D3D1),
+                  ),
+                ),
+              ),
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
